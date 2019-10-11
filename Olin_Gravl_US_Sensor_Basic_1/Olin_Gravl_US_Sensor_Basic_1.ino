bb6e8a32998e5f43d0d301ac47e88d5b137ef341
LSanten <leon.santen@icloud.com>
2019-10-11 17:41:25 -0400

    Serial communication between teensys works steadily

--- Olin_Gravl_US_Sensor_Basic_1/Olin_Gravl_US_Sensor_Basic_1.ino
+++ Olin_Gravl_US_Sensor_Basic_1/Olin_Gravl_US_Sensor_Basic_1.ino
@@ -8,12 +8,22 @@
  Trig:  Measurement trigger pin - Low level pulse over 50us triggers one distance measurement.
  Holding this pin LOW will repeats the measurement every 100ms.
  */
+ /*
+ OUT: analog output 0.33mV equals 1mm
+ */
+
 #define  TRIGGER   2
 #define  LED       13
 
-/*
- OUT: analog output 0.33mV equals 1mm
- */
+
+
+uint32_t US_frequency = 15; //frequency: clicks per second
+uint32_t current_millis; 
+uint32_t task_millis = 0;
+uint32_t task_length = 1000/US_frequency;
+uint32_t serial_freq_time;
+
+float measured_serial_freq;
 
 int number_of_sensors = 3; //the amount of ultrasonic sensors used
 int sensor_pins[] = {15, 16, 18}; // input pins for sensor data - pin number refers to sensor from left to right in direction of traffic
@@ -29,52 +39,42 @@ void setup()
 }
 void loop()
 {
-  // generate the pulse to trigger the sensor
-  digitalWrite(TRIGGER, LOW);
-  delayMicroseconds(50);
-  digitalWrite(TRIGGER, HIGH);
-  delayMicroseconds(50);
+  current_millis = millis();
 
-  unsigned int sensor_data[number_of_sensors];
-
-  for (int i=0; i < number_of_sensors; i++){
-    sensor_data[i] = analogRead(sensor_pins[i]);
-    sensor_data[i] = (unsigned int)(((unsigned long)sensor_data[i] * 5000)/1024); //change from analog value to voltage
-    sensor_data[i] = (unsigned int)((float)sensor_data[i] / 0.33f); //0.33mV equals 1mm
-  }
-
-  //char sensor_data_char = char(sensor_data);
-  //size_t data_size = sizeof(sensor_data_char);
-  //Serial1.write(sensor_data_char, data_size);
-  //Serial1.print(sensor_data);
-  // TODO --> PRINT OUT TIME BETWEEN SENT AND RECEIVED SIGNALS TO CHECK IF SERIAL PRINT LAPTOP IS SLOW OR IF COMMUNICATION IS SLOW
+  // send trigger and run code in if statement with frequency --> US_frequency
+  if (current_millis - task_millis >= task_length){
+    task_millis = millis();
+    // generate the pulse to trigger the sensor
+    digitalWrite(TRIGGER, LOW);
+    delayMicroseconds(50);
+    digitalWrite(TRIGGER, HIGH);
+    delayMicroseconds(50);
   
-  /*
-  // Serial print FLAG when value from sensors is higher than flag_value
-  for (int i=0; i < number_of_sensors; i++){
-    if (sensor_data[i] <= flag_value) {
-      Serial1.print("  FLAG-");
-      Serial1.print(i + 1);
+    unsigned int sensor_data[number_of_sensors];
+    String data_string; // string that contains data
+
+    // loop through sensors, read data, convert to mm, print data
+    data_string.concat('{');
+    for (int i=0; i < number_of_sensors; i++){
+      sensor_data[i] = analogRead(sensor_pins[i]);
+      sensor_data[i] = (unsigned int)(((unsigned long)sensor_data[i] * 5000)/1024); //change from analog value to voltage
+      sensor_data[i] = (unsigned int)((float)sensor_data[i] / 0.33f); //0.33mV equals 1mm      
+      
+      
+      data_string.concat(sensor_data[i]);
+      if (i < number_of_sensors - 1){
+        data_string.concat(',');
+      }
+      else{
+        data_string.concat('}');
+      }
     }
+    // Print sensor data like (in mm):
+    // {1581,2115,1551}
+    Serial1.println(data_string);
+    
+    measured_serial_freq = 1000/(millis() - serial_freq_time); // calc frequency with current time and time from last loop (serial_freq_time)
+    serial_freq_time = millis(); // take current time for next freq measurement
+    
   }
-  Serial1.println();
-  */
-
-  /*
-  // Print out all sensor values
-  for (int i=0; i < number_of_sensors; i++){
-    Serial1.print("sensor ");
-    Serial1.print(sensor_pins[i]);
-    Serial1.print(": ");
-    Serial1.print(sensor_data[i]);
-    Serial1.print("mm");
-    Serial1.print(" | ");
-  }
-
-  Serial1.println();
-  */
-  
-  digitalWrite(LED, LOW);
-  delay(100); // TODO: remove delay function
-  digitalWrite(LED, HIGH);
 }
--- Olin_Gravl_US_Sensor_Serial_Second_Teensy/Olin_Gravl_US_Sensor_Serial_Second_Teensy.ino
+++ Olin_Gravl_US_Sensor_Serial_Second_Teensy/Olin_Gravl_US_Sensor_Serial_Second_Teensy.ino
@@ -3,6 +3,10 @@
 int led = 13;
 uint32_t FLAG_time; // millis() time when flag was raised for turning on/off FLAG led (13)
 
+uint32_t serial_freq_time;
+
+float measured_serial_freq;
+
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
@@ -10,29 +14,21 @@ void setup() {
 }
 
 void loop() {
-        String incomingByte;
-/**        
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    digitalWrite(led, HIGH);
-    Serial.print("USB received: ");
-    Serial.println(incomingByte, DEC);
-                HWSERIAL.print("USB received:");
-                HWSERIAL.println(incomingByte, DEC);
-  }
-**/
+  String incomingByte;
+        
   if (Serial2.available() > 0) {
-    incomingByte = Serial2.readString();
+    incomingByte = Serial2.read();
     digitalWrite(led, HIGH);
     FLAG_time = millis();
     //Serial.print("Serial2 received: ");
+
+    measured_serial_freq = 1000/(millis() - serial_freq_time); // calc frequency with current time and time from last loop (serial_freq_time)
+    serial_freq_time = millis(); // take current time for next freq measurement
+    
     Serial.print(incomingByte);
     //Serial.println(incomingByte, DEC);
     //            HWSERIAL.println("Serial2 received:");
     //            HWSERIAL.println(incomingByte);
   }
-     
-  
-  
-  
+  digitalWrite(led, LOW);
 }
